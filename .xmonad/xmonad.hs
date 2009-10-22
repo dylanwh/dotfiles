5a921e5eb5cbe8e5930bd9e046189d18695582cb
Dylan William Hardison <dylan@hardison.net>
2009-10-22 13:17:39 -0400

    homedir cleanup.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -7,7 +7,6 @@ import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
 import XMonad.Actions.DwmPromote
-import XMonad.Actions.FindEmptyWorkspace
 import XMonad.Actions.CycleRecentWS
 import XMonad.Actions.CycleWS
 import XMonad.Actions.SinkAll
@@ -24,13 +23,12 @@ import XMonad.Prompt.Directory
 import XMonad.Prompt.Shell
 import XMonad.Prompt.Ssh
 import XMonad.Prompt.XMonad
-import XMonad.Prompt.Layout
 
 import XMonad.Layout.Grid
 import XMonad.Layout.IM
 import XMonad.Layout.LayoutHints
 import XMonad.Layout.Magnifier
-import XMonad.Layout.Maximize
+-- import XMonad.Layout.Maximize
 import XMonad.Layout.Named
 import XMonad.Layout.NoBorders
 import XMonad.Layout.PerWorkspace
@@ -38,7 +36,6 @@ import XMonad.Layout.Reflect (reflectHoriz)
 import XMonad.Layout.Tabbed
 import XMonad.Layout.WorkspaceDir
 import XMonad.Layout.LayoutCombinators
-
 
 import XMonad.Util.EZConfig
 
@@ -54,24 +51,24 @@ import Text.XHtml (tag, strAttr, renderHtml, (<<), (!), primHtml)
 -- }}}
 
 -- Misc {{{
+wsHome   = "home"
+wsWeb    = "web"
+wsGimp   = "gimp"
+wsDocs   = "docs"
+wsVM     = "vm"
 
-wsHome   = wsIndex 1
-wsWeb    = wsIndex 4
-wsGimp   = wsIndex 6
-wsOffice = wsIndex 5
-wsVM     = wsIndex 7
-
-wsList  = ["home", "code", "work", "web", "docs", "gimp", "vm" ]
-wsIndex i = wsList !! (i-1)
+wsList  = ["home", "code", "work", "fs", "docs", "vm", "gimp", "misc", "web" ]
+wsKeys  = zip (map show [1 .. 9]) wsList
 
 spawnExec str = spawn ("exec " ++ str)
 
+home path = do dir <- io $ getEnv "HOME" `catch` const (return "/")
+               return (dir ++ '/' : path)
 -- }}}
 
 -- {{{ main
 main = do
-    let myFont       = "-xos4-terminus-bold-r-*-*-*-140-100-100-*-*-iso8859-1"
-
+    let myFont     = "-xos4-terminus-bold-r-*-*-*-140-100-100-*-*-iso8859-1"
     let myXPConfig = defaultXPConfig 
             { font        = myFont
             , height      = 24
@@ -97,23 +94,21 @@ main = do
     let myKeys = 
             [ ("M-`",              spawnExec $ terminal myConfig)
             , ("M-c",              kill)
+            , ("M-<Page_Up>",      prevWS)
+            , ("M-<Page_Down>",    nextWS)
             , ("M-<Return>",       dwmpromote)
             , ("M-S-<Return>",     windows W.focusMaster)
-            , ("M-S-b",            sendMessage ToggleStruts)
+            , ("M-b",              sendMessage ToggleStruts)
+            , ("M-m",              sendMessage Toggle)
+--            , ("M-S-m",          withFocused (sendMessage . maximizeRestore))
             , ("M-s",              sshPrompt      myXPConfig)
             , ("M-p",              scriptPrompt   myXPConfig)
             , ("M-S-p",            shellPrompt    myXPConfig)
             , ("M-o",              bookmarkPrompt myXPConfig)
             , ("M-d",              changeDir      myXPConfig)
-            , ("M-m",              withFocused (sendMessage . maximizeRestore))
-            , ("M-S-m",            sendMessage Toggle)
-            , ("M-S-t",            sinkAll)
-            , ("M-n",              viewEmptyWorkspace)
-            , ("M-S-n",            tagToEmptyWorkspace)
             , ("M-<Tab>",          cycleRecentWS [xK_Super_L] xK_Tab xK_grave)
-            , ("M-x",              layoutPrompt myXPConfig)
-            , ("M-9",              windows (W.greedyView wsWeb))
-            ] 
+            ] ++ [ ("M-"   ++ k,   windows (W.greedyView n))  | (k, n) <- wsKeys ]
+              ++ [ ("M-S-" ++ k,   windows (W.shift      n))  | (k, n) <- wsKeys ]
 
     xmonad $ myConfig `additionalKeysP` myKeys
 -- }}}
@@ -163,9 +158,9 @@ myManageHook = composeAll
     , resource  =? "irc"                --> doF (W.shift wsHome)
     , resource  =? "rss"                --> doF (W.shift wsHome)
     , resource  =? "shell_fm"           --> doF (W.shift wsHome)
-    , className =? "Xpdf"               --> doF (W.shift wsOffice)
-    , className =? "OpenOffice.org 2.4" --> doF (W.shift wsOffice)
-    , resource  =? "OpenOffice.org"     --> doF (W.shift wsOffice)
+    , className =? "Xpdf"               --> doF (W.shift wsDocs)
+    , className =? "OpenOffice.org 2.4" --> doF (W.shift wsDocs)
+    , resource  =? "OpenOffice.org"     --> doF (W.shift wsDocs)
     , className =? "Firefox-bin"        --> doF (W.shift wsWeb)
     , className =? "Firefox"            --> doF (W.shift wsWeb)
     , className =? "Iceweasel"          --> doF (W.shift wsWeb)
@@ -189,35 +184,37 @@ myLayoutHook = workspaceDir "~"
              $ onWorkspace wsVM   full
              $ onWorkspace wsGimp gimp
              $ onWorkspace wsWeb  full
-             $ onWorkspace wsOffice (full ||| float)
+             $ onWorkspace wsDocs (full ||| float)
              $ tall ||| Mirror tall ||| grid ||| full
   where
      -- default tiling algorithm partitions the screen into two panes
      tall = named "Tall" 
           $ layoutHints
-          $ maximize
+          $ magnifier'
           $ Tall nmaster delta ratio
 
      -- default grid
      grid = named "Grid" 
           $ layoutHints
-          $ maximize
+          $ magnifierOff
           $ Grid
 
      -- im layout
      im   = named "IM" 
-          -- $ reflectHoriz 
+          $ layoutHints
+          $ magnifierOff
           $ withIM (1%6) (Title "Buddy List") 
           $ Grid
 
      gimp = named "Gimp"
+          $ layoutHints
           $ withIM (0.13) (Role "gimp-toolbox")
           $ reflectHoriz 
           $ withIM (0.20) (Role "gimp-dock")
           $ Full
 
      -- floating layout, renamed
-     float = named "Float" $ layoutHints $ maximize $  simpleFloat
+     float = named "Float" $ layoutHints $ simpleFloat
 
      -- full layout, renamed.
      full = named "Full" $ smartBorders $ layoutHints Full
@@ -237,10 +234,6 @@ scriptPrompt conf = do-- {{{
     dirExecPromptNamed conf spawnExec dir  "Script: "
 -- }}}
 
-
-home :: String -> IO String-- {{{
-home path = do dir <- io $ getEnv "HOME" `catch` const (return "/")
-               return (dir ++ '/' : path) -- }}}
 
 data Bookmark = Bookmark-- {{{
 
