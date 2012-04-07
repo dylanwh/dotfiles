0fe6b18c14a73d62acddbe242a800fea33e1440f
Dylan William Hardison <dylan@hardison.net>
2012-04-06 22:10:46 -0400

    working gnome3 + xmonad setup

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,50 +8,40 @@ import System.Exit
 import qualified XMonad.StackSet as W
 import qualified Data.Map        as M
 
+import qualified XMonad.Actions.PhysicalScreens as PS
 import XMonad.Actions.DwmPromote
-import XMonad.Actions.SinkAll
 import XMonad.Actions.GridSelect
 import XMonad.Actions.UpdatePointer
--- import XMonad.Actions.DynamicWorkspaces
-import XMonad.Actions.FocusNth
-import XMonad.Actions.CycleWS
-import XMonad.Hooks.FadeInactive
-import qualified XMonad.Actions.PhysicalScreens as PS
+import XMonad.Actions.WindowMenu
 
-import XMonad.Config.Desktop
+import XMonad.Config.Gnome (gnomeRegister)
 
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops (ewmh)
+import XMonad.Hooks.ICCCMFocus
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.Minimize
+import XMonad.Hooks.RestoreMinimized
+import XMonad.Hooks.SetWMName
+
+import XMonad.Layout.Fullscreen
+import XMonad.Layout.Minimize
+import XMonad.Layout.Maximize
+import XMonad.Layout.LayoutCombinators
+import XMonad.Layout.LayoutHints
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.NoBorders
 
 import XMonad.Prompt
-import XMonad.Prompt.DirExec
-import XMonad.Prompt.Directory
 import XMonad.Prompt.Shell
-import XMonad.Prompt.Ssh
-import XMonad.Prompt.AppLauncher
-
-import XMonad.Layout.LayoutModifier
-import XMonad.Layout.Grid
-import XMonad.Layout.IM
-import XMonad.Layout.LayoutHints
-import XMonad.Layout.Named
-import XMonad.Layout.NoBorders
-import XMonad.Layout.Reflect (reflectHoriz)
-import XMonad.Layout.LayoutCombinators
-import XMonad.Layout.OneBig
-import XMonad.Layout.Fullscreen
-import qualified XMonad.Layout.Fullscreen as FS
 
 import XMonad.Util.EZConfig
-import XMonad.Util.Scratchpad
 import XMonad.Util.Run
+import XMonad.Util.SpawnOnce
 
-import Control.Arrow ((>>>), second)
 import Control.Monad (liftM, liftM2)
 import Data.Char
-import Data.List (isPrefixOf)
 import Data.Ratio ((%))
 import Data.Maybe
 
@@ -60,29 +50,7 @@ import Data.Bits ((.&.))
 import XMonad.Util.WindowProperties (getProp32s)
 
 import System.Environment (getEnv)
-import System.IO (hPutStrLn, hClose, hFlush)
--- }}}
-
--- Misc {{{
-spawnExec str = spawn ("exec " ++ str ++ " &> /dev/null")
-
-home path = do dir <- io $ getEnv "HOME" `catch` const (return "/")
-               return (dir ++ '/' : path)
-
-progs = [ "capture-screen"
-        , "chrome"
-        , "chrome -p work"
-        , "dterm"
-        , "eclipse"
-        , "firefox"
-        , "lofn-chat"
-        , "lofn-mail"
-        , "midori"
-        , "pwsafe-query"
-        , "skype"
-        , "xdo-paste"
-        ]
--- }}}
+{- }}} -}
 
 -- {{{ main
 main = do
@@ -97,54 +65,57 @@ main = do
             , fgHLight    = "black"
             }
 
-    let myGSConfig x = x { gs_cellheight = 30
-                         , gs_cellwidth = 150 
-                         , gs_cellpadding = 5
+    let myGSConfig x = x { gs_cellheight  = 26
+                         , gs_cellwidth   = 150 
+                         , gs_cellpadding = 1
+                         , gs_font        = "-xos4-terminus-bold-r-*-*-*-120-100-100-*-*-iso8859-1"
                          }
 
-    let myGSConfigWin = myGSConfig $ buildDefaultGSConfig fromClassName
-    let myGSConfigStr = myGSConfig $ defaultGSConfig { gs_navigate = navNSearch }
+    let gsWindows    = myGSConfig $ buildDefaultGSConfig fromClassName
+    let gsPrograms   = myGSConfig $ defaultGSConfig { gs_navigate = navNSearch }
+    let gsWorkspaces = myGSConfig $ defaultGSConfig { gs_navigate = navNSearch }
 
-    let dc = desktopConfig
-    let myConfig = dc
+    let myConfig = defaultConfig
             { borderWidth        = 2
             , terminal           = "dterm"
             , normalBorderColor  = "#000033"
             , focusedBorderColor = "red"
             , workspaces         = map show [1..9]
             , modMask            = mod4Mask
-            , manageHook         = fullscreenManageHook <+>  myManageHook <+> manageHook dc
-            , logHook            = myLogHook <+> logHook desktopConfig
-            , handleEventHook    = fullscreenEventHook <+> handleEventHook dc
-            , layoutHook         = desktopLayoutModifiers (fullscreenFull myLayoutHook)
-            , startupHook        = myStartupHook >> startupHook dc
+            , manageHook         = fullscreenManageHook <+> myManageHook
+            , logHook            = myLogHook
+            , handleEventHook    = myHandleEventHook
+            , layoutHook         = myLayoutHook
+            , startupHook        = myStartupHook
             , focusFollowsMouse  = True
             }
+
     let myKeys = 
             [ ("M-`",              spawnExec $ terminal myConfig)
-            , ("M-<Escape>",       spawnExec $ terminal myConfig)
+            , ("M-<Escape>",       programMenu gsPrograms)
             , ("M-<Return>",       dwmpromote)
-            , ("M-S-t",            withFocused $ windows . W.sink)
-            , ("M-s",              sshPrompt      myXPConfig)
-            , ("M-p",              spawnSelected myGSConfigStr progs)
-            , ("M-S-p",            shellPrompt    myXPConfig)
-            , ("M-o",              bookmarkPrompt myXPConfig)
-            , ("M-q",              spawn "true")
-            , ("M-g",              goToSelected myGSConfigWin)
-            , ("M-b",            bringSelected myGSConfigWin)
+            , ("M-p",              programMenu gsPrograms)
+            , ("M-S-p",            shellPrompt myXPConfig)
+            , ("M-g",              goToSelected gsWindows)
+            , ("M-b",              bringSelected gsWindows)
             , ("M-S-c",            kill)
+            , ("M-q",              spawn "true")
             , ("M-w",              PS.viewScreen 0)
             , ("M-e",              PS.viewScreen 1)
             , ("M-S-w",            PS.sendToScreen 0)
             , ("M-S-e",            PS.sendToScreen 1)
-            , ("M-0",              gridselectWorkspace myGSConfigStr W.greedyView)
+            , ("M-0",              gridselectWorkspace gsWorkspaces W.greedyView)
             , ("M-f",              sendMessage ToggleStruts)
+            , ("M-o",              windowMenu)
+            , ("M-S-v",            spawn "xdo-paste")
+            , ("M-S-q",            spawn "gnome-session-quit --no-prompt")
             ] 
 
-    let conf = myConfig `additionalKeysP` myKeys
-    xmonad =<< dzen conf
+    let conf = ewmh (myConfig `additionalKeysP` myKeys)
 
--- }}}
+    -- hack to make minecraft work?
+    xmonad {- =<< dzen -} conf { logHook = logHook conf >> setWMName "LG3D" }
+{- }}} -}
 
 -- {{{ manage hook:
 -- Execute arbitrary actions and WindowSet manipulations when managing
@@ -155,50 +126,39 @@ main = do
 -- To find the property name associated with a program, use
 -- > xprop | grep WM_CLASS
 -- and click on the client you're interested in.
-myManageHook = composeOne
-             [ transience
-             , title     =? "Add to Panel"       -?> doFloat
-             , resource  =? "gnome-panel"        -?> doIgnore
-             , title     =? "Factor workspace"   -?> doFloat
-             , className =? "Glade-3"            -?> doFloat
-             , className =? "WMClock"            -?> doIgnore
-             , className =? "stalonetray"        -?> doIgnore
-             , className =? "kxdocker"           -?> doIgnore
-             , className =? "trayer"             -?> doIgnore
-             , className =? "Google-chrome"      -?> doShift "web"
-             , resource  =? "pwsafe-prompt"      -?> doFloat
-             , resource  =? "desktop_window"     -?> doIgnore
-             , resource  =? "tint2"              -?> doIgnore
-             , resource  =? "kdesktop"           -?> doIgnore
-             , resource  =? "panel"              -?> doIgnore
-             , resource  =? "chat"               -?> viewShift "chat"
-             , resource  =? "kicker"             -?> doIgnore
-             , myIsFullscreen                    -?> doFullFloat
-             ]
-    where viewShift = doF . liftM2 (.) W.view W.shift
--- }}}
+myManageHook = fullscreenManageHook <+> manageDocks <+> rules
+    where rules = composeOne
+                [ transience
+                , title     =? "Add to Panel"       -?> doFloat
+                , resource  =? "gnome-panel"        -?> doIgnore
+                , title     =? "Factor workspace"   -?> doFloat
+                , className =? "Glade-3"            -?> doFloat
+                , className =? "WMClock"            -?> doIgnore
+                , className =? "stalonetray"        -?> doIgnore
+                , className =? "kxdocker"           -?> doIgnore
+                , className =? "trayer"             -?> doIgnore
+                , className =? "Google-chrome"      -?> doShift "web"
+                , resource  =? "pwsafe-prompt"      -?> doFloat
+                , resource  =? "desktop_window"     -?> doIgnore
+                , resource  =? "tint2"              -?> doIgnore
+                , resource  =? "kdesktop"           -?> doIgnore
+                , resource  =? "panel"              -?> doIgnore
+                , resource  =? "kicker"             -?> doIgnore
+                , myIsFullscreen                    -?> doFullFloat
+                ]
+{- }}} -}
 
--- {{{ layout hook:
-myLayoutHook = lessBorders (Combine Difference Screen OnlyFloat)
-             $ (tall ||| Mirror tall ||| onebig ||| grid ||| full)
+{- {{{ layout hook: -}
+myLayoutHook = smartBorders 
+             $ fullscreenFull
+             $ avoidStruts
+             $ layoutHintsToCenter
+             $ maximize
+             $ minimize
+             $ tall ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
-     tall = named "Tall" 
-          $ layoutHintsToCenter
-          $ Tall nmaster delta ratio
-
-     -- default grid
-     grid = named "Grid" 
-          $ layoutHintsToCenter
-          $ Grid
-
-     -- full layout, renamed.
-     full = named "Full" 
-          $ Full
-
-     onebig = named "OneBig"
-            $ layoutHints 
-            $ OneBig (3/4) (3/4)
+     tall = Tall nmaster delta ratio
 
      -- The default number of windows in the master pane
      nmaster = 1
@@ -208,42 +168,36 @@ myLayoutHook = lessBorders (Combine Difference Screen OnlyFloat)
 
      -- Percent of screen to increment by when resizing panes
      delta   = 2/100
--- }}}
+{- }}} -}
 
-myLogHook = do home <- io $ getEnv "HOME"
-               updatePointer (Relative 0.5 0.5)
+{- {{{ log hook -}
+myLogHook = do updatePointer (Relative 0.5 0.5)
+               status <- dynamicLogString dzenPP
+               xmonadPropLog status
+               takeTopFocus
+{- }}} -}
 
-scriptPrompt conf = do-- {{{
-    dir <- io $ home "/.xmonad/scripts"
-    dirExecPromptNamed conf spawnExec dir  "Script: "
--- }}}
+{- {{{ startup hook: -}
+myStartupHook = do xmonadrc <- home ".xmonad/xmonadrc"
+                   setWMName "LG3D"
+                   spawnOnce xmonadrc
+                   gnomeRegister
+                   
+{- }}} -}
 
-data Bookmark = Bookmark-- {{{
+myHandleEventHook = fullscreenEventHook 
+                <+> minimizeEventHook 
+                <+> restoreMinimizedEventHook 
 
-instance XPrompt Bookmark where
-    showXPrompt Bookmark = "Bookmark: "
-    completionToCommand _ = id
 
-bookmarkPrompt :: XPConfig -> X ()
-bookmarkPrompt c = do
-    file  <- io $ getEnv "BOOKMARKS" `catch` const (home ".surfraw.bookmarks")
-    marks <- io $ getBookmarks file
-    mkXPrompt Bookmark c (mkComplFunFromList' (map fst marks)) (gotoBookmark marks)
+-- utilility functions {{{
+spawnExec str = spawn ("exec " ++ str ++ " &> /dev/null")
 
-gotoBookmark :: [(String, String)] -> String -> X ()
-gotoBookmark marks name =
-    case lookup name marks of
-         Just url -> spawnExec ("url-handler " ++ url)
-         Nothing  -> return ()
-
-getBookmarks :: String -> IO [(String, String)]
-getBookmarks file = do
-    text <- readFile file
-    return (map pair (filter (not.null) (lines text)))
-
-pair :: String -> (String, String)
-pair = break isSpace >>> second (dropWhile isSpace)
--- }}}
+home path = do dir <- io $ getEnv "HOME" `catch` const (return "/")
+               return (dir ++ '/' : path)
+programMenu gs = do progs_file <- home ".xmonad/programs"
+                    progs      <- liftM lines (io $ readFile progs_file)
+                    spawnSelected gs progs
 
 myIsFullscreen = do w <- ask
                     fs <- isFullscreen
@@ -252,7 +206,4 @@ myIsFullscreen = do w <- ask
                                           case p of 
                                                Just (flags:_:decorations:_) -> return ((flags .&. 2) /= 0 && decorations == 0)
                                                Nothing -> return False
-
-
-myStartupHook = do setWMName "LG3D"
-                   spawn "be trayer"
+-- }}}
