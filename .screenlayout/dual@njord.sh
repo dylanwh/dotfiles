800a7e48c58b98f2935600d40e79e625f41eebec
Dylan William Hardison <dylan@hardison.net>
2013-01-28 03:02:26 -0500

    utilities for messing with screen resolutions

---
+++ .screenlayout/dual@njord.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output DVI-I-1 --mode 1600x900 --pos 0x0 --rotate right --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 900x300 --rotate normal
---
+++ .screenlayout/single@njord.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-0 --off --output DVI-I-1 --off --output DVI-I-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal
