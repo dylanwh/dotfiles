54213f7951743d91a433fd76b1330b48b5ecc358
Dylan William Hardison <dylan@hardison.net>
2013-10-18 11:56:13 -0400

    bin: fix error in find-bad-submodules

--- bin/find-bad-submodules.pl
+++ bin/find-bad-submodules.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use 5.18.0;
+use 5.16.0;
 use Path::Class::Rule;
 use Config::GitLike;
 use YAML::XS;
@@ -11,25 +11,25 @@ my $modules = parse(Config::GitLike->load_file('~/.gitmodules'), 'submodule');
 
 for my $path (keys %$config) {
     unless (exists $modules->{$path}) {
-        warn '$path does not exist in ~/.gitmodules\n';
+        warn "$path does not exist in ~/.gitmodules\n";
         print "git config --remove-section $config->{$path}{SECTION}\n";
         next;
     }
     unless ($modules->{$path}{url} eq $config->{$path}{url}) {
-        warn '$path url disagrees between .gitmodules and .git/config\n';
+        warn "$path url disagrees between .gitmodules and .git/config\n";
     }
 }
 
 for my $path (keys %$modules) {
     unless (exists $config->{$path}) {
-        warn '$path does not exist in ~/.git/config\n';
+        warn "$path does not exist in ~/.git/config\n";
         next;
     }
     unless ($path eq $modules->{$path}{path}) {
-        warn '$path inconsistent in .gitmodules\n';
+        warn "$path inconsistent in .gitmodules\n";
     }
     unless ($modules->{$path}{url} eq $config->{$path}{url}) {
-        warn '$path url disagrees between .gitmodules and .git/config\n';
+        warn "$path url disagrees between .gitmodules and .git/config\n";
     }
 }
 
