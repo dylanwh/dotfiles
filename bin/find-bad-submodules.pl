f497551407ee6c3c7263b7371f4c689ea82636d2
Dylan William Hardison <dylan@hardison.net>
2013-10-18 12:00:05 -0400

    bin: detect more problems

--- bin/find-bad-submodules.pl
+++ bin/find-bad-submodules.pl
@@ -6,30 +6,34 @@ use Path::Class::Rule;
 use Config::GitLike;
 use YAML::XS;
 
-my $config  = parse(Config::GitLike->load_file('~/.git/config'), 'submodule');
-my $modules = parse(Config::GitLike->load_file('~/.gitmodules'), 'submodule');
+my $config  = parse(Config::GitLike->load_file('.git/config'), 'submodule');
+my $modules = parse(Config::GitLike->load_file('.gitmodules'), 'submodule');
 
 for my $path (keys %$config) {
     unless (exists $modules->{$path}) {
         warn "$path does not exist in ~/.gitmodules\n";
         print "git config --remove-section $config->{$path}{SECTION}\n";
-        next;
     }
     unless ($modules->{$path}{url} eq $config->{$path}{url}) {
         warn "$path url disagrees between .gitmodules and .git/config\n";
+    }
+    unless (-d $path) {
+        warn "$path does not exist\n";
     }
 }
 
 for my $path (keys %$modules) {
     unless (exists $config->{$path}) {
         warn "$path does not exist in ~/.git/config\n";
-        next;
     }
     unless ($path eq $modules->{$path}{path}) {
         warn "$path inconsistent in .gitmodules\n";
     }
     unless ($modules->{$path}{url} eq $config->{$path}{url}) {
         warn "$path url disagrees between .gitmodules and .git/config\n";
+    }
+    unless (-d $path) {
+        warn "$path does not exist\n";
     }
 }
 
@@ -40,8 +44,8 @@ sub parse {
 
     for (grep { /^$qprefix\./ } keys %$hash) {
         my ($section, $key) = /^$qprefix\.(.*?)\.(\w+)$/;
-        $result{$section}{$key} = $hash->{$_};
-        $result{$section}{KEY}  = $_;
+        $result{$section}{$key}    = $hash->{$_};
+        $result{$section}{KEY}     = $_;
         $result{$section}{SECTION} = "$prefix.$section";
     }
 
