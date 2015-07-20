fef49a37943cc47dfe63611422a84a92f597540c
Dylan William Hardison <dylan@hardison.net>
2015-07-20 00:48:02 -0400

    weekend hacks

--- .offlineimap.py
+++ .offlineimap.py
@@ -1,12 +1,12 @@
+import subprocess
 
+def get_output(cmd):
+    # Bunch of boilerplate to catch the output of a command:
+    pipe = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    (output, errout) = pipe.communicate()
+    assert pipe.returncode == 0 and not errout
+    return output
 
-
-# translation rules
-# INBOX             rename inbox
-# [Gmail]/Sent Mail rename sent
-# [Gmail]/Important rename important
-# [Gmail]/Trash     skip
-# [Gmail]/Spam      skip
-# */archive         skip
-# spam              copy_to [Gmail]/Spam
-
+def get_password_emacs(host, port):
+    cmd = "emacsclient --eval '(offlineimap-get-password \"%s\" \"%s\")'" % (host,port)
+    return get_output(cmd).strip().lstrip('"').rstrip('"')
