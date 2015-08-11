3dc89fa4e90e96101873faa692e469fda630b4f8
Dylan William Hardison <dylan@hardison.net>
2015-08-11 16:22:02 -0400

    emacs stuff

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,78 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(compilation-message-face (quote default))
- '(cua-global-mark-cursor-color "#2aa198")
- '(cua-normal-cursor-color "#839496")
- '(cua-overwrite-cursor-color "#b58900")
- '(cua-read-only-cursor-color "#859900")
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "2e5705ad7ee6cfd6ab5ce81e711c526ac22abed90b852ffaf0b316aa7864b11f" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "823ea71cd79048ec98ba0bd131d969fa51ff595f8bdb25640b92e84653d72fb6" "a776135e3d68ebb9c5033799a86290e2243e352f5b8fe6b3b96fbf80c65acd0c" "40a783fd65a506a532c32423cd5ca3619d8192cac030b1f1cc2a609d7521bc9d" "6a9606327ecca6e772fba6ef46137d129e6d1888dcfc65d0b9b27a7a00a4af20" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
- '(evil-mode-line-format (quote before))
- '(evil-want-fine-undo nil)
- '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors
-   (--map
-    (solarized-color-blend it "#002b36" 0.25)
-    (quote
-     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
- '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors
-   (quote
-    (("#073642" . 0)
-     ("#546E00" . 20)
-     ("#00736F" . 30)
-     ("#00629D" . 50)
-     ("#7B6000" . 60)
-     ("#8B2C02" . 70)
-     ("#93115C" . 85)
-     ("#073642" . 100))))
- '(hl-bg-colors
-   (quote
-    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
- '(hl-fg-colors
-   (quote
-    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
- '(magit-diff-use-overlays nil)
- '(magit-use-overlays nil)
- '(org-agenda-files
-   (quote
-    ("~/org/bugzilla.org" "~/.emacs.d/config.org" "/Users/dylan/org/elastic-quick-search.org" "/Users/dylan/org/from-mobile.org" "/Users/dylan/org/notes.org" "~/org/elastic-quick-search.org" "~/org/notes.org")))
- '(org-babel-load-languages (quote ((emacs-lisp . t) (perl . t) (sql . t) (js . t))))
- '(org-confirm-babel-evaluate nil)
- '(org-confirm-elisp-link-function nil)
- '(org-default-notes-file "~/org/notes.org")
- '(org-log-done (quote time))
- '(org-log-into-drawer t)
- '(org-mobile-directory "/Volumes/files/org/")
- '(org-mobile-use-encryption nil)
- '(org-modules
-   (quote
-    (org-bbdb org-bibtex org-docview org-gnus org-info org-mouse org-protocol org-rmail org-w3m org-bookmark org-bullets org-eshell org-eval org-favtable org-man)))
- '(org-open-directory-means-index-dot-org t)
- '(org-refile-allow-creating-parent-nodes (quote confirm))
- '(org-return-follows-link t)
- '(org-src-fontify-natively t)
- '(org-tab-follows-link t)
- '(org-tags-column 100)
- '(pos-tip-background-color "#073642")
- '(pos-tip-foreground-color "#93a1a1")
- '(show-paren-mode t)
- '(smartparens-global-mode nil)
- '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
- '(sml/mode-width (quote right))
- '(solarized-distinct-fringe-background t)
- '(solarized-high-contrast-mode-line nil)
- '(solarized-scale-org-headlines nil)
- '(solarized-use-less-bold nil)
- '(solarized-use-more-italic t)
- '(solarized-use-variable-pitch nil)
- '(term-default-bg-color "#002b36")
- '(term-default-fg-color "#839496")
- '(weechat-color-list
-   (quote
-    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
