9c11b15e32a0d2742ac0ff91e8206a28174d492e
Dylan William Hardison <dylan@hardison.net>
2017-09-01 01:37:09 -0400

    only add dirs that exist

--- .config/fish/functions/initfish.fish
+++ .config/fish/functions/initfish.fish
@@ -1,12 +1,17 @@
 function initfish
-	colorload
-abbrload
-set -U fish_user_paths $HOME/bin $HOME/.plenv/shims $HOME/.cargo/bin $HOME/.gem/ruby/2.0.0/bin
-if set -q $RUST_SRC_PATH
-  have rustc
-  and set -l rust_sysroot (rustc --print sysroot)
-  test -n $rust_sysroot
-  and test -d "$rust_sysroot/lib/rustlib/src/rust/src"
-  and set -xU RUST_SRC_PATH "$rust_sysroot/lib/rustlib/src/rust/src"
-end
+    colorload
+    abbrload
+    set -U fish_user_paths (
+      for dir in $HOME/bin $HOME/.plenv/shims $HOME/.cargo/bin $HOME/.gem/ruby/2.0.0/bin
+        test -d $dir
+        and echo $dir
+      end
+    )
+    if set -q $RUST_SRC_PATH
+        have rustc
+        and set -l rust_sysroot (rustc --print sysroot)
+        test -n $rust_sysroot
+        and test -d "$rust_sysroot/lib/rustlib/src/rust/src"
+        and set -xU RUST_SRC_PATH "$rust_sysroot/lib/rustlib/src/rust/src"
+    end
 end
