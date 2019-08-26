7ea93be1c7b4690d1752f6c04fda5dffb5ee2e1e
Dylan William Hardison <dylan@hardison.net>
2019-08-26 13:45:37 -0400

    ssh cleanup

--- .config/fish/functions/wtb.fish
+++ .config/fish/functions/wtb.fish
@@ -1,29 +1,28 @@
-function wtb
+function wtb -a branch
     if not test -e .git
         echo "wtb must be run from a dir with a .git file or directory" >&2
         return 1
     end
-
-    set -l branch $argv[1]
-    set -l prefix (git repo-name)
-    set -l dir "../$prefix-$branch"
 
     if test -z "$branch"
         echo "Usage: wtb branch-name" >&2
         return 1
     end
 
+    set -l prefix (git repo-name)
+    set -l dir "../$prefix-$branch"
+
     if test -d "$dir"
         echo "directory $dir already exists" >&2
         return 1
     end
 
-    git branch $branch
+    git branch dylan/$branch
     or return 1
 
-    git worktree add $dir $branch
+    git worktree add $dir dylan/$branch
     or return 1
 
-    code $dir
+    cd $dir
     or return 1
 end
