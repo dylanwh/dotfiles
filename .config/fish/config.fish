c4bba4f443d3557d4b5a2c79d1ce7eea464193c0
Dylan Hardison <dylan@hardison.net>
2020-04-21 21:32:18 -0400

    fix path stuff on macs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,16 @@
+set -x REALNAME "Dylan William Hardison"
+set -x EMAIL "dylan@hardison.net"
+set -x MANPAGER 'less -s'
+set -x LANG en_US.UTF-8
+set -x LC_COLLATE POSIX # sort in POSIX order.
+set -x TZ US/Eastern
+set -x GOPATH $HOME/go
+
+if [ -x /usr/libexec/path_helper ]
+    source (/usr/libexec/path_helper -c | psub)
+end
+set -U fish_user_paths $fish_user_paths
+
 set -l config_version (stat -c %Y ~/.config/fish/functions/apply-fish-defaults.fish)
 
 test -z "$dylan_config_version"
@@ -6,35 +19,29 @@ and apply-fish-defaults
 
 set -U dylan_config_version $config_version
 
-set -x REALNAME "Dylan William Hardison"
-set -x EMAIL "dylan@hardison.net"
-set -x MANPAGER 'less -s'
-set -x LANG en_US.UTF-8
-set -x LC_COLLATE POSIX # sort in POSIX order.
-set -x TZ US/Eastern
-
-switch "$TERM_PROGRAM"
-    case vscode
-        set code code
-        if string match -q "*insider" $TERM_PROGRAM_VERSION
-            abbr -a -g -- code code-insiders
-            set code code-insiders
-        end
-        set -x EDITOR "$code -w"
-        abbr -a -g -- vi $code
-        abbr -a -g -- vim $code
-    case '*'
-        set -x ALTERNATE_EDITOR ""
-        set -x EDITOR "emacsclient -t"
-        abbr -a -g vi emacs
-end
-
-for env_file in plenv pyenv chef brew
-    test -f ~/.config/fish/{$env_file}.fish
-    and source ~/.config/fish/{$env_file}.fish
-end
 
 if status --is-interactive
+    switch "$TERM_PROGRAM"
+        case vscode
+            set code code
+            if string match -q "*insider" $TERM_PROGRAM_VERSION
+                abbr -a -g -- code code-insiders
+                set code code-insiders
+            end
+            set -x EDITOR "$code -w"
+            abbr -a -g -- vi $code
+            abbr -a -g -- vim $code
+        case '*'
+            set -x ALTERNATE_EDITOR ""
+            set -x EDITOR "emacsclient -t"
+            abbr -a -g vi emacs
+    end
+
+    for env_file in plenv pyenv chef brew
+        test -f ~/.config/fish/{$env_file}.fish
+        and source ~/.config/fish/{$env_file}.fish
+    end
+
     set -U BASE16_SHELL "$HOME/.config/base16-shell"
     if test -d $BASE16_SHELL
         function base16-load
@@ -48,9 +55,8 @@ if status --is-interactive
         set -gx BASE16_THEME (string match 'base16-*' $BASE16_THEME  | string sub -s (string length 'base16-*'))
         eval sh '"'(realpath ~/.base16_theme)'"'
     end
+
+    test -f {$HOME}/.iterm2_shell_integration.fish
+    and source {$HOME}/.iterm2_shell_integration.fish
 end
 
-
-
-test -f {$HOME}/.iterm2_shell_integration.fish
-and source {$HOME}/.iterm2_shell_integration.fish
--- .config/fish/functions/apply-fish-defaults.fish
+++ .config/fish/functions/apply-fish-defaults.fish
@@ -4,18 +4,17 @@ function apply-fish-defaults --description 'apply one-time fish configuration st
     set -U fish_greeting ''
 
     path clear
-
-    path add /snap/bin
     path add ~/bin
-    path add ~/.plenv/bin
-    path add ~/.pyenv/bin
-    path add ~/.plenv/shims
-    path add ~/.pyenv/shims
+    for env in pyenv plenv
+        path add ~/.$env/bin
+        path add ~/.$env/shims
+    end
+    path add $GOPATH/bin
+    path add /snap/bin
     path add ~/.cargo/bin
     path add /opt/chefdk/bin
     path add ~/.chefdk/gem/ruby/2.5.0/bin
     path add /opt/chefdk/embedded/bin
-    path add (go env GOPATH)/bin
     path prune
 
     source (dircolors -c | sed 's/setenv/set -Ux/' | psub)
