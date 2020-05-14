5bb7ebbef8716a5c4c05ef33730c9065be6fb723
Dylan Hardison <dylan@hardison.net>
2020-05-13 23:44:08 -0400

    foo

--- .config/fish/functions/apply-fish-defaults.fish
+++ .config/fish/functions/apply-fish-defaults.fish
@@ -6,16 +6,6 @@ function apply-fish-defaults --description 'apply one-time fish configuration st
     path clear
     path default
 
-    set --erase -g LS_COLORS
-    set --erase -U LS_COLORS
-    for dircolors in dircolors gdircolors
-        if have $dircolors
-            $dircolors -c ~/.dircolors | sed 's/setenv/set -Ux/' | source
-            set -Ux LS_COLORS "$LS_COLORS:ow=1;7;34:st=30;44:su=30;41"
-        end
-    end
-    test -z $LS_COLORS
-    and echo "no dircolors"
-    colorload
+    selenized
     abbrload
 end
