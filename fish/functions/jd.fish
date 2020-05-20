2ce1a59fbaa56219d384ca41087688bbe9b3e28c
Dylan Hardison <dylan@hardison.net>
2020-05-20 00:32:14 -0400

    I should sleep

--- fish/functions/jd.fish
+++ fish/functions/jd.fish
@@ -1,11 +1,17 @@
-function jd --argument dir
+function jd
+    argparse "a/all" -- $argv
+    set dir $argv[1]
+    set -l fd_args "-td"
     set -l fzy_args
     if [ -n "$dir" ]
-        set fzy_args -q $dir
+        set -a fzy_args -q $dir
+    end
+    if [ $_flag_all ]
+        set -a fd_args "-H"
     end
     set dir (begin
         echo ".."
-        fd -Htd
+        fd $fd_args
     end | fzy $fzy_args)
     and cd $dir
     return
