bcd3ca46c8e6fcc0361add37433799f075b53048
Dylan Hardison <dylan@hardison.net>
2020-05-20 12:31:03 -0400

    rewrite git-organized

--- fish/functions/jg.fish
+++ fish/functions/jg.fish
@@ -3,7 +3,7 @@ function jg --argument term
     test -n "$term"
     and set fzy_args -q $term
 
-    pushd ~/Git/github.com
+    pushd ~/Git
     set dir (fd -Htd .git | sed 's/\.git$//' | fzy $fzy_args)
     and cd $dir
     or popd
