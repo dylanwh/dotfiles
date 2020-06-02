3ad14054182437c2db4de4675d207f826958467c
Dylan Hardison <dylan@hardison.net>
2020-06-01 20:01:08 -0400

    add ctrl-t preview options

--- fish/functions/hame.fish
+++ fish/functions/hame.fish
@@ -7,6 +7,7 @@ function hame
     set -Ux SKIM_DEFAULT_COMMAND 'fd --type f'
     set -Ux SKIM_ALT_C_COMMAND 'fd -L -t d -E /sys -E /proc -E /dev -E /tmp'
     set -Ux SKIM_CTRL_T_COMMAND 'fd -L -t f -t d -t l -E /sys -E /proc -E /dev -E /tmp'
+    set -Ux SKIM_CTRL_T_OPTS '--preview \'bat --color always {1}; or exa --color always {1}\''
 
     set -g fish_user_path $fish_user_path
     path clear
