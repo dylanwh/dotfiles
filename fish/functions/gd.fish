8aff5703a6d4742e7c4b99ab4ba87d434acf65fd
Dylan Hardison <dhardison@fastly.com>
2020-09-15 13:00:25 -0400

    improve gd command

--- fish/functions/gd.fish
+++ fish/functions/gd.fish
@@ -1,4 +1,7 @@
-function gd
-set dir (list-github-repos | sk -p 'cd> ')
-and cd ~/Git/$dir
+# Defined in /var/folders/ch/9q4qpbxn3yxczpxmqzpb4xjw0000gn/T//fish.TfflIJ/gd.fish @ line 1
+function gd --argument query
+    test -n "$query"
+    and set sk_args -q "$query"
+    set dir (list-github-repos | sk $sk_args -p 'cd> ')
+    and cd ~/Git/$dir
 end
