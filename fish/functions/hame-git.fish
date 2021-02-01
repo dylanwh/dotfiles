cf6c30683a0301ded5977ba82cdc5a22b16016e1
Dylan Hardison <dhardison@fastly.com>
2021-02-01 17:03:13 -0500

    add alias to git delta navigate

--- fish/functions/hame-git.fish
+++ fish/functions/hame-git.fish
@@ -9,10 +9,12 @@ function hame-git
    git config --global alias.up "!git fetch --all --tags --prune && git pull --rebase"
    git config --global alias.reword "commit --amend"
    git config --global alias.top "!pwd"
+   git config --global alias.ndiff '!env DELTA_NAVIGATE=1 git diff'
    git config --global color.ui auto
    git config --global color.diff auto
    git config --global color.status auto
    git config --global color.branch auto
+   git config --global diff.colorMoved default
    git config --global core.excludesfile "~/.cvsignore"
    git config --global pull.ff only
    if have delta
