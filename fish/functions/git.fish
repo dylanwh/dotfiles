11abfa4b3c92c2f03d00129b5d4f1c84ebee0065
Dylan Hardison <dylan@hardison.net>
2021-03-01 16:32:18 -0500

    make git work even if hub is not installed

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,4 +1,10 @@
 # Defined in - @ line 1
-function git --wraps=hub --description 'alias git hub'
-  hub  $argv;
+if have hub
+    function git --wraps=hub --description 'alias git hub'
+        hub  $argv;
+    end
+else
+    function git --wraps=git --description 'alias git hub'
+        command git $argv
+    end
 end
