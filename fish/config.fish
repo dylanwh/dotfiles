687c70eccafa32d010d1d4199b8ab9b3d8fd34c3
Dylan Hardison <dhardison@fastly.com>
2021-04-16 22:28:27 -0400

    fish fixups

--- fish/config.fish
+++ fish/config.fish
@@ -13,37 +13,37 @@ and source (env -i /usr/libexec/path_helper -c | psub)
 
 set -U fish_user_paths $fish_user_paths
 
-set -g shell_parent (ps -o ppid= $fish_pid | xargs ps -o comm=)
-set --erase shell_via
-
-switch $shell_parent
-    case 'mosh*'
-        set -x shell_via mosh
-    case 'sshd*'
-        smart-ssh-agent
-    case 'login' '/usr/bin/login'
-        switch $OS
-            case Darwin
-                ssh-add -q -A
-                or for file in ~/.ssh/id_ed25519 ~/.ssh/id_rsa
-                    ssh-add -K $file
-                end
-        end
-end
-
-switch $shell_parent
-    case 'ssh*' 'mosh*'
-        if have caffeinate; and have nq
-            mkdir -p $HOME/.cache
-            # I'm not sure why, but if I don't run nq inside nq, it sometimes fails to clean up its files
-            # Might be a bug, or something about how macOS handles the session leader being teriminated?
-            # Anyway, this pretty much ensures remove logins prevent the mac from going to sleep
-            # (as long as it is running on AC)
-            env "NQDIR=$HOME/.cache/caffinate.$fish_pid" nq -c -q -- nq -c -q -- caffeinate -s -w $fish_pid
-        end
-end
-
 if status --is-interactive
+    set -g shell_parent (ps -o ppid= $fish_pid | xargs ps -o comm=)
+    set --erase shell_via
+
+    switch $shell_parent
+        case 'mosh*'
+            set -x shell_via mosh
+        case 'sshd*'
+            smart-ssh-agent
+        case 'login' '/usr/bin/login'
+            switch $OS
+                case Darwin
+                    ssh-add -q -A
+                    or for file in ~/.ssh/id_ed25519 ~/.ssh/id_rsa
+                        ssh-add -K $file
+                    end
+            end
+    end
+
+    switch $shell_parent
+        case 'ssh*' 'mosh*'
+            if have caffeinate; and have nq
+                mkdir -p $HOME/.cache
+                # I'm not sure why, but if I don't run nq inside nq, it sometimes fails to clean up its files
+                # Might be a bug, or something about how macOS handles the session leader being teriminated?
+                # Anyway, this pretty much ensures remove logins prevent the mac from going to sleep
+                # (as long as it is running on AC)
+                env "NQDIR=$HOME/.cache/caffinate.$fish_pid" nq -c -q -- nq -c -q -- caffeinate -s -w $fish_pid
+            end
+    end
+
     switch "$TERM_PROGRAM"
         case vscode
             set code code
