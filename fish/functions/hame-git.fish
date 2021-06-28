55a45ff44e506a56368e42bb1dd35a77288f8c54
Dylan Hardison <dhardison@fastly.com>
2021-06-28 13:16:01 -0400

    fixup gitconfig

--- fish/functions/hame-git.fish
+++ fish/functions/hame-git.fish
@@ -1,23 +1,26 @@
 function hame-git
     hame-echo configuring git
-    git config --global push.default simple
-    git config --global user.email $EMAIL
-    git config --global user.name "$REALNAME"
-    git config --global alias.co checkout
-    git config --global alias.ci commit
-    git config --global alias.br branch
-    git config --global alias.st "status --short --branch"
-    git config --global alias.up "!git fetch --all --tags --prune && git pull --rebase"
-    git config --global alias.reword "commit --amend"
-    git config --global alias.top "!pwd"
-    git config --global alias.ndiff '!env DELTA_NAVIGATE=1 git diff'
-    git config --global color.ui auto
+    git config --global color.branch auto
     git config --global color.diff auto
     git config --global color.status auto
-    git config --global color.branch auto
-    git config --global diff.colorMoved default
+    git config --global color.ui auto
     git config --global core.excludesfile "~/.cvsignore"
+    git config --global diff.colorMoved default
+    git config --global init.defaultBranch main
     git config --global pull.ff only
+    git config --global push.default simple
+    git config --global user.email "$EMAIL"
+    git config --global user.name "$REALNAME"
+
+    git config --global alias.br branch
+    git config --global alias.ci commit
+    git config --global alias.co checkout
+    git config --global alias.ndiff '!env DELTA_NAVIGATE=1 git diff'
+    git config --global alias.reword "commit --amend"
+    git config --global alias.st "status --short --branch"
+    git config --global alias.top "!pwd"
+    git config --global alias.up "!git fetch --all --tags --prune && git pull --rebase"
+
     if have delta
         hame-echo configuring git to use delta
         git config --global core.pager (which delta)
