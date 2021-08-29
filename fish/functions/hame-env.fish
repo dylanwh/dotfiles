ee1abca2da930881cb181be1a953f8d383803031
Dylan Hardison <dylan@hardison.net>
2021-08-29 00:02:04 -0400

    make sure to export *_ROOT for /opt/env

--- fish/functions/hame-env.fish
+++ fish/functions/hame-env.fish
@@ -1,30 +1,30 @@
 function hame-env
-    set -U plenv_root $HOME/.plenv
-    set -U pyenv_root $HOME/.pyenv
-    set -U nodenv_root $HOME/.nodenv
+    set -Ux PLENV_ROOT $HOME/.plenv
+    set -Ux PYENV_ROOT $HOME/.pyenv
+    set -Ux NODENV_ROOT $HOME/.nodenv
 
     if [ -d /opt/env ]
         for env in plenv pyenv nodenv
-            set $env"_root" /opt/env/$env
+            set -Ux (string upper $env)_ROOT /opt/env/$env
         end
     end
 
-    hame-clone -n plenv      -p $plenv_root                     -u 'tokuhirom/plenv.git'
-    hame-clone -n perl-build -p $plenv_root/plugins/perl-build  -u 'tokuhirom/perl-build.git'
-    hame-clone -n pyenv      -p $pyenv_root                     -u 'pyenv/pyenv.git'
-    hame-clone -n nodenv     -p $nodenv_root                    -u 'nodenv/nodenv.git'
-    hame-clone -n node-build -p $nodenv_root/plugins/node-build -u 'nodenv/node-build.git'
+    hame-clone -n plenv      -p $PLENV_ROOT                     -u 'tokuhirom/plenv.git'
+    hame-clone -n perl-build -p $PLENV_ROOT/plugins/perl-build  -u 'tokuhirom/perl-build.git'
+    hame-clone -n pyenv      -p $PYENV_ROOT                     -u 'pyenv/pyenv.git'
+    hame-clone -n nodenv     -p $NODENV_ROOT                    -u 'nodenv/nodenv.git'
+    hame-clone -n node-build -p $NODENV_ROOT/plugins/node-build -u 'nodenv/node-build.git'
 
-    path add $plenv_root/bin
-    path add $plenv_root/shims
+    path add $PLENV_ROOT/bin
+    path add $PLENV_ROOT/shims
     plenv init - fish | grep -v 'set -gx PATH' > ~/.config/fish/functions/plenv.fish
 
-    path add $pyenv_root/bin
-    path add $pyenv_root/shims
+    path add $PYENV_ROOT/bin
+    path add $PYENV_ROOT/shims
     pyenv init - fish | grep -v 'set -gx PATH' > ~/.config/fish/functions/pyenv.fish
 
-    path add $nodenv_root/bin
-    path add $nodenv_root/shims
+    path add $NODENV_ROOT/bin
+    path add $NODENV_ROOT/shims
     nodenv init - fish | grep -v 'set -gx PATH' > ~/.config/fish/functions/nodenv.fish
 
 end
