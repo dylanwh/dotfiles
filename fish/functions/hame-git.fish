04b8f8c5e527d91e0d0724fdd7a0ccc20d33c2fb
Dylan Hardison <dhardison@fastly.com>
2022-02-04 13:05:00 -0500

    updates to git custom commands

--- fish/functions/hame-git.fish
+++ fish/functions/hame-git.fish
@@ -12,7 +12,7 @@ function hame-git
     git config --global user.email "$EMAIL"
     git config --global user.name "$REALNAME"
 
-    git config --global alias.br branch
+    git config --global alias.br branches
     git config --global alias.ci commit
     git config --global alias.co checkout
     git config --global alias.ndiff '!env DELTA_NAVIGATE=1 git diff'
