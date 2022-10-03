ea7f7e4c59b93f685b65504775c0854dc51689eb
Dylan Hardison <dhardison@fastly.com>
2022-10-03 09:30:37 -0700

    update stuff

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,6 +1,6 @@
 if have exa
     alias ls "exa -Fh --group-directories-first"
-else if command ls --group-directories-first 2>&1 | grep -q illegal
+else if command ls --group-directories-first 2>&1 | egrep -q 'illegal|unrecognized'
     gnu_alias ls "-Fh --color=auto --group-directories-first"
     or alias ls "ls -GFh"
 else
