4ca815eb2b02158da0bde88b2dfff121edc39cc3
Dylan Hardison <dhardison@fastly.com>
2022-10-24 16:37:58 -0700

    hame-vim: support updating

--- fish/functions/hame-clone.fish
+++ fish/functions/hame-clone.fish
@@ -1,13 +1,21 @@
 function hame-clone
-    argparse 'n/name=' 'p/path=' 'u/url=' 'b/branch=' -- $argv
+    argparse 'n/name=' 'p/path=' 'u/url=' 'b/branch=' 'U/update' -- $argv
     or return 1
 
     if not [ -n "$_flag_path" -a -n "$_flag_url" -a "$_flag_name" ]
+        echo "Usage: hame-clone [-n name] [-p path] [-u url] [-b branch] [-U]" 2>&1
         return 2
     end
 
     pushd $HOME
-    if ! [ -d "$_flag_path/.git" ]
+    if [ -d "$_flag_path/.git" ]
+        if [ -n "$_flag_update" ]
+            pushd $_flag_path
+            hame-echo git pull $_flag_name
+            command git pull
+            popd
+        end
+    else
         set -l parent (dirname $_flag_path)
         if [ "$parent" != "." ]
             mkdir -vp $parent
