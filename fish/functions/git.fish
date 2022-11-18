4856ec4bbb28a9e1b1443c498d9126ecbc02be00
Dylan Hardison <dylan@hardison.net>
2022-11-17 19:51:55 -0800

    fix hub

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,9 +1,10 @@
-if status is-interactive
+# Defined in - @ line 1
+if have hub
     function git --wraps=hub --description 'alias git hub'
-        hub  $argv
+        hub  $argv;
     end
 else
-    function git
+    function git --wraps=git --description 'alias git hub'
         command git $argv
     end
 end
