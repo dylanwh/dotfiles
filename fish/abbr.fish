dfe568ea054556151d5b4bc102a76ae63a8b08ae
Dylan Hardison <dhardison@fastly.com>
2023-05-04 11:54:57 -0700

    version guard

--- fish/abbr.fish
+++ fish/abbr.fish
@@ -17,25 +17,29 @@ abbr runti 'docker run --rm -ti'
 abbr upstream 'git push --set-upstream origin (git branch --show-current)'
 abbr hm history merge
 
-function gh_abbr -a prefix
-    set match (string match -g -r 'gh:([^/]+)/(.*)' $prefix)
-    set user $match[1]
-    set repo $match[2]
-    echo "git@github.com:$user/$repo.git"
+set -l ver (string split . $FISH_VERSION)
+# fish 3.6.0 or later
+if [ $ver[1] -ge 3 -a $ver[2] -ge 6 ]
+    function gh_abbr -a prefix
+        set match (string match -g -r 'gh:([^/]+)/(.*)' $prefix)
+        set user $match[1]
+        set repo $match[2]
+        echo "git@github.com:$user/$repo.git"
+    end
+
+    function vim_edit
+        echo vim $argv
+    end
+
+    abbr -a vim_edit_texts --position command --regex ".+\.txt" --function vim_edit
+
+    abbr -a github_prefix \
+        --position anywhere \
+        --regex 'gh:[^/]+/[^/]+' \
+        --function gh_abbr
+
+    function last_history_item
+        echo $history[1]
+    end
+    abbr -a !! --position anywhere --function last_history_item
 end
-
-function vim_edit
-    echo vim $argv
-end
-
-abbr -a vim_edit_texts --position command --regex ".+\.txt" --function vim_edit
-
-abbr -a github_prefix \
-    --position anywhere \
-    --regex 'gh:[^/]+/[^/]+' \
-    --function gh_abbr
-
-function last_history_item
-    echo $history[1]
-end
-abbr -a !! --position anywhere --function last_history_item
