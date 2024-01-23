c36840b9fac4e08c2b3f9ec28683a7d217d4f0c0
Dylan Hardison <dylan@hardison.net>
2024-01-22 22:03:21 -0800

    remove unused fish functions

--- fish/functions/proxycmd.fish
+++
@@ -1,4 +0,0 @@
-function proxycmd --argument host cmd
-echo -e "#!/bin/sh\nssh $host $cmd" | sudo tee /usr/local/bin/$cmd > /dev/null
-sudo chmod 755 /usr/local/bin/$cmd
-end
--- fish/functions/zreload.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function zreload --description 'alias zreload=exec fish'
-	exec fish $argv;
-end
