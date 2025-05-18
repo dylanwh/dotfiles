74b7b0d625eb0d5f07f6acd032b9e89c909fba2a
Dylan Hardison <dylan@hardison.net>
2025-05-18 01:22:49 -0700

    more kitty updates

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,7 +1,7 @@
 if have eza
-    alias ls "eza -F -h --group-directories-first"
+    alias ls "eza -F -h --group-directories-first --hyperlink"
 else if command ls --group-directories-first 2>&1 | egrep -q 'illegal|unrecognized'
-    gnu_alias ls "-Fh --color=auto --group-directories-first"
+    gnu_alias ls "-Fh --color=auto --group-directories-first --hyperlink"
     or alias ls "ls -GFh"
 else
     alias ls "ls -Fh --color=auto --group-directories-first"
--- kitty/keys.py
+++ kitty/keys.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 
 for i in range(1, 10):
-    tab = i if i < 10 else 0
-    print(f"map cmd+{i} goto_tab {tab}")
+    print(f"map cmd+{i} goto_tab {i}")
