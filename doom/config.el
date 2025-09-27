4ef71bd33bc7fb3df5b0df880c0ea01a4c634c9e
Dylan Hardison <dylan@hardison.net>
2025-09-26 21:11:11 -0700

    rm doom quit and fix bookmarks saves

--- doom/config.el
+++ doom/config.el
@@ -112,6 +112,9 @@
       '("[" (:eval system-name) "] "
         "emacs %b"))
 
+(setq bookmark-default-file "~/.doom.d/bookmarks")
+(setq bookmark-save-flag 1)
+
 ;; Global Auto-Revert Mode is a global minor mode that reverts any
 ;; buffer associated with a file when the file changes on disk.  Use
 ;; auto-revert-mode to revert a particular buffer.
