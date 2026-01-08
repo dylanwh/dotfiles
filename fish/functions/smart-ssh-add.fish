25e6539285c206751853c88916bea8ef31296082
Dylan Hardison <dylan@hardison.net>
2026-01-07 21:51:22 -0800

    use 1password agent when available

--- fish/functions/smart-ssh-add.fish
+++ fish/functions/smart-ssh-add.fish
@@ -8,10 +8,14 @@ function smart-ssh-add
                 and ssh-add --apple-use-keychain $file
             end
         case '*'
-            ssh-add -q
-            or for file in ~/.ssh/id_ed25519 ~/.ssh/id_rsa
-                test -f $file
-                and ssh-add $file
+            if test -S ~/.1password/agent.sock
+                set -Ux SSH_AUTH_SOCK $HOME/.1password/agent.sock
+            else
+                ssh-add -q
+                or for file in ~/.ssh/id_ed25519 ~/.ssh/id_rsa
+                    test -f $file
+                    and ssh-add $file
+                end
             end
     end
 end
