66f04aff1ef517819179fd4b7c650d551bde8e62
Dylan Hardison <dhardison@fastly.com>
2026-01-20 17:02:06 +0000

    fix LD_LIBRARY_PATH

--- nix/home.nix
+++ nix/home.nix
@@ -1,36 +1,39 @@
-{ config, pkgs, lib, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
 let
-  # Import the packages module
   packagesModule = import ./packages.nix { inherit config pkgs lib; };
-  
-  # Extract system packages directly from the module's attribute
-  systemPackages = packagesModule.environment.systemPackages or [];
-  
-  # Define conflicting package pairs (keep first, remove second)
+  systemPackages = packagesModule.environment.systemPackages or [ ];
+
   conflicts = {
-    rustup = [ "cargo" "rustc" ];
+    rustup = [
+      "cargo"
+      "rustc"
+    ];
     psmisc = [ "pstree" ];
-    # Add more conflicts as needed
   };
-  
-  # Get list of package names present
+
   presentPackages = builtins.map (pkg: pkg.pname or "") systemPackages;
-  
-  # Determine which packages to exclude based on conflicts
+
   excludedPackages = lib.flatten (
-    lib.mapAttrsToList (keeper: conflicting:
-      if builtins.elem keeper presentPackages
-      then conflicting
-      else []
+    lib.mapAttrsToList (
+      keeper: conflicting: if builtins.elem keeper presentPackages then conflicting else [ ]
     ) conflicts
   );
-  
-  # Filter out conflicting packages
-  deduplicatedPackages = builtins.filter 
-    (pkg: !(builtins.elem (pkg.pname or "") excludedPackages))
-    systemPackages;
 
-in {
+  deduplicatedPackages = builtins.filter (
+    pkg: !(builtins.elem (pkg.pname or "") excludedPackages)
+  ) systemPackages;
+
+in
+{
   home.packages = deduplicatedPackages;
+
+  home.sessionVariables = {
+    LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib/"; # fix the problem of dynamic link in python package
+  };
 }
