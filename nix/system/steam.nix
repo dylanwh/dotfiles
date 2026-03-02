790e3e1b69c40200c8ec0e431386a35267dcc56b
Dylan Hardison <dylan@hardison.net>
2026-03-01 23:05:30 -0800

    remove steam customizations

--- nix/system/steam.nix
+++ nix/system/steam.nix
@@ -6,34 +6,9 @@
 }:
 
 {
-  programs.steam =
-    let
-      patchedBwrap = pkgs.bubblewrap.overrideAttrs (o: {
-        patches = (o.patches or [ ]) ++ [
-          ../patches/bwrap2.patch
-        ];
-      });
-    in
-    {
-      enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-
-      package = pkgs.steam.override {
-        buildFHSEnv = (
-          args:
-          (
-            (pkgs.buildFHSEnv.override {
-              bubblewrap = patchedBwrap;
-            })
-            (
-              args
-              // {
-                extraBwrapArgs = (args.extraBwrapArgs or [ ]) ++ [ "--cap-add ALL" ];
-              }
-            )
-          )
-        );
-      };
-    };
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
 }
