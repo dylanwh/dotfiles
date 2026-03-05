5528ab0bd82517a0253a07ffa16be5dc55a731ae
Dylan Hardison <dylan@hardison.net>
2026-03-04 19:31:39 -0800

    add config for odin

---
+++ nix/configuration-odin.nix
@@ -0,0 +1,44 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
+
+{
+  imports = [
+    ./packages.nix
+    ./system/macos.nix
+    <home-manager/nix-darwin>
+  ];
+
+  home-manager.users.dylan =
+    { pkgs, ... }:
+    {
+      imports = [
+        ./home/bash.nix
+        ./home/emacs.nix
+        ./home/fish.nix
+        ./home/git.nix
+        ./home/karabiner.nix
+        ./home/kitty.nix
+        ./home/local-bin.nix
+        ./home/macos.nix
+        ./home/mail.nix
+        ./home/misc.nix
+        ./home/selenized.nix
+        ./home/ssh.nix
+        ./home/starship.nix
+        ./home/tmux.nix
+        ./home/vim.nix
+      ];
+
+      # The state version is required and should stay at the version you
+      # originally installed.
+      home.stateVersion = "25.11";
+    };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 6;
+}
